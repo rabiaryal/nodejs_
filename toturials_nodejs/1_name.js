84dc4112c53584667fcf612e76d7954e18632a44
itsrabii <142924291+rabiaryal@users.noreply.github.com>
2024-12-24 00:39:50 +0545

    node js+ express basic

---
+++ toturials_nodejs/1_name.js
@@ -0,0 +1,5 @@
+const secret = "Super Secret";
+const john = "john";
+const peter = "peter";
+
+module.exports = { john, peter };
---
+++ toturials_nodejs/app.js
@@ -0,0 +1,13 @@
+const names = require('./1_name')
+const {john, peter} = require('./1_name')
+const sayHi = require('./2_utlis')
+// console.log(names);
+// sayHi("rabi");
+
+// sayHi('rabi');
+// sayHi(names.john);
+// sayHi(john);
+// sayHi(peter)
+// sayHi(names.peter)
+
+require('./3_add_values')
